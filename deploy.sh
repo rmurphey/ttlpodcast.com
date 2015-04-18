86d10b242b87fd371f252406fc0e7655ab44b99b
Rebecca Murphey <rebecca.murphey@bazaarvoice.com>
2015-04-18 14:07:05 -0500

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# generate the static assets
 harp compile
+
+# use personal aws credentials
 . ../aws-credentials
-aws s3 cp www/ s3://ttlpodcast.com/ --recursive
+
+# sync everything except files that should have a short ttl
+aws s3 sync www/ s3://ttlpodcast.com --cache-control="max-age=1576800000" \
+  --exclude="index.html" \
+  --exclude="rss.xml"
+
+# sync files that should have a short ttl
+aws s3 sync www/index.html s3://ttlpodcast.com/index.html --cache-control="max-age=600" \
+  --exclude="*" \
+  --include="index.html" \
+  --include="rss.xml"
